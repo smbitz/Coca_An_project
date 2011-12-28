5df9bcad05b6c42bae6868aa9d9681fdf3fffdc2
Chetchaiyan <Chetchaiyan@hotmail.com>
2011-12-28 16:58:36 +0700

    return data from BuildDialog.java to GameActivity.java create BuildItem.java UI

--- src/codegears/coca/dialog/BuildDialog.java
+++ src/codegears/coca/dialog/BuildDialog.java
@@ -6,13 +6,18 @@ import codegears.coca.MyApp;
 import codegears.coca.R;
 import codegears.coca.data.Item;
 import codegears.coca.data.Player;
+import codegears.coca.ui.BuildItem;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.HorizontalScrollView;
+import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class BuildDialog extends Activity implements OnClickListener {
 	
@@ -22,7 +27,7 @@ public class BuildDialog extends Activity implements OnClickListener {
 	private MyApp app;
 	
 	private ImageButton closeButton;
-	private HorizontalScrollView shopItemScrollView;
+	private LinearLayout itemLayout;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +39,47 @@ public class BuildDialog extends Activity implements OnClickListener {
 		
 		closeButton = (ImageButton) this.findViewById(R.id.plantCloseButton);
 		closeButton.setOnClickListener(this);
-		shopItemScrollView = (HorizontalScrollView) this.findViewById(R.id.shopItemScrollView);
+		LinearLayout itemLayout = (LinearLayout)this.findViewById( R.id.plantItem );
+		BuildItem test = new BuildItem(this);
+		test.setLayoutParams( new LinearLayout.LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT ) );
+		test.setItemName("Hello Item Name");
+		BuildItem hello = new BuildItem(this);
+		hello.setLayoutParams( new LinearLayout.LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT ) );
+		itemLayout.addView( test );
+		itemLayout.addView( hello );
+
 	}
 
 	@Override
 	public void onClick(View v) {
 		if( v.equals(closeButton) ){
 			this.finish();
+		}
+	}
+	
+	private class GridAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			return 12;
+		}
+
+		@Override
+		public Object getItem( int position ) {
+			return position;
+		}
+
+		@Override
+		public long getItemId( int position ) {
+			return position;
+		}
+
+		@Override
+		public View getView( int position, View convertView, ViewGroup parent ) {
+			Button b = new Button(BuildDialog.this);
+			b.setLayoutParams( new GridView.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT) );
+			b.setText( "test grid" );
+			return b;
 		}
 	}
 }
